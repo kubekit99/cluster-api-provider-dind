3c1cd1c91926da997702aa35e8e86e6aced0cd3b
Jerome Brette <jb447c@att.com>
2019-02-03 13:01:34 -0600

    Remove bin and vendor from git

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # cluster-api-provider-dind
+
 POC of cluster api using Docker in Docker
+
+Quick notes taken during the creation of the cluster-api-provider-dind scaffolding creation
+
+## Prerequesist
+
+Install dep
+
+```bash
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+sudo mv $HOME/bin/dep /usr/bin
+```
+
+Install kustomize
+
+```bash
+wget https://github.com/kubernetes-sigs/kustomize/releases/download/v1.0.11/kustomize_1.0.11_linux_amd64
+mv kustomize_1.0.11_linux_amd64 kustomize
+chmod +x kustomize 
+sudo mv kustomize /usr/bin/
+```
+
+Install kubebuilder (contains a copy of kubectl, etcd and kubeapi-server)
+
+```bash
+wget https://github.com/kubernetes-sigs/kubebuilder/releases/download/v1.0.8/kubebuilder_1.0.8_linux_amd64.tar.gz
+tar xvf kubebuilder_1.0.8_linux_amd64.tar.gz 
+mv kubebuilder_1.0.8_linux_amd64 kubebuilder
+sudo mv kubebuilder /usr/local
+```
+
+Ensure the local kubelete and kubernetes cluster is stopped
+
+```bash
+sudo systemctl stop kubelet
+docker stop $(docker ps -qa)
+export PATH=$PATH:/usr/local/kubebuilder/bin
+```
+
+## Creation of scaffolding
+
+### Generation steps
+
+```bash
+cd cluster-api-provider-dind/
+```
+
+```bash
+kubebuilder init --domain kubekit.cloud --license apache2 --owner "The Kubernetes Authors"
+git add .
+git commit -m "Generate scaffolding."
+git push
+```
+
+```bash
+kubebuilder create api --group dind --version v1alpha1 --kind AirshipClusterProviderSpec
+kubebuilder create api --group dind --version v1alpha1 --kind AirshipClusterProviderStatus
+kubebuilder create api --group dind --version v1alpha1 --kind AirshipMachineProviderStatus
+kubebuilder create api --group dind --version v1alpha1 --kind AirshipMachineProviderSpec
+vi cmd/manager/main.go 
+mkdir -p pkg/cloud/dind/actuators/cluster/
+mkdir -p pkg/cloud/dind/actuators/machine/
+vi pkg/cloud/dind/actuators/cluster/actuator.go
+vi pkg/cloud/dind/actuators/machine/actuator.go
+vi pkg/controller/add_cluster_controller.go
+vi pkg/controller/add_machine_controller.go
+vi Makefile 
+dep ensure
+export IMG=kubekit99/cluster-api-provider-dind
+dep ensure
+make
+make docker-build IMG=${IMG}
+git add .
+git commit -m "Add CRDs and build image"
+git push
+```
+
+### Cleanup steps
+
+If you used brute force, git add . the repository becomes bulky. In order to keep the
+scrict minimum
+
+```bash
+git rm -fr vendor/
+git rm -fr bin/
+git commit
+git push origin --force --all
+```
+
+```bash
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch vendor" HEAD
+git push origin --force --all
+git clone https://github.com/kubekit99/cluster-api-provider-dind.git
+```
+
+```bash
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch bin" HEAD
+git push origin --force --all
+git clone https://github.com/kubekit99/cluster-api-provider-dind.git
+```
+
+## Rebuild images
+
+List of commands used to rebuild and publish the images.
+
+```bash
+export IMG=kubekit99/cluster-api-provider-dind
+dep ensure
+make
+make docker-build IMG=${IMG}
+make docker-push IMG=${IMG}
+```
+
+## Deployment and tests
+
+List of commands to deploy and test.
+
+Ensure that /usr/local/kubebuilder is no longer in your path if you are using the same machine
+
+
+```bash
+kubectl get nodes
+kubectl apply -f provider-components.yaml
+kubectl get all --all-namespaces
+kubectl logs pod/cluster-api-provider-dind-controller-manager-0 -n cluster-api-provider-dind-system manager
+```
+
+TBD
+
+
+
--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -17,68 +17,80 @@ limitations under the License.
 package main
 
 import (
-	"flag"
-	"os"
+        "flag"
+        "fmt"
+        "os"
 
-	"github.com/kubekit99/cluster-api-provider-dind/pkg/apis"
-	"github.com/kubekit99/cluster-api-provider-dind/pkg/controller"
-	"github.com/kubekit99/cluster-api-provider-dind/pkg/webhook"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
-	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+        "github.com/kubekit99/cluster-api-provider-dind/pkg/apis"
+        "github.com/kubekit99/cluster-api-provider-dind/pkg/cloud/dind/actuators/cluster"
+        "github.com/kubekit99/cluster-api-provider-dind/pkg/cloud/dind/actuators/machine"
+        clusterapis "sigs.k8s.io/cluster-api/pkg/apis"
+        "sigs.k8s.io/cluster-api/pkg/apis/cluster/common"
+        "sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset"
+        capicluster "sigs.k8s.io/cluster-api/pkg/controller/cluster"
+        capimachine "sigs.k8s.io/cluster-api/pkg/controller/machine"
+        "sigs.k8s.io/controller-runtime/pkg/client/config"
+        "sigs.k8s.io/controller-runtime/pkg/manager"
+        logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+        "sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 )
 
 func main() {
+        cfg := config.GetConfigOrDie()
+        if cfg == nil {
+                panic(fmt.Errorf("GetConfigOrDie didn't die"))
+        }
+
 	var metricsAddr string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.Parse()
-	logf.SetLogger(logf.ZapLogger(false))
-	log := logf.Log.WithName("entrypoint")
+        flag.Parse()
+        log := logf.Log.WithName("dind-controller-manager")
+        logf.SetLogger(logf.ZapLogger(false))
+        entryLog := log.WithName("entrypoint")
 
-	// Get a config to talk to the apiserver
-	log.Info("setting up client for manager")
-	cfg, err := config.GetConfig()
-	if err != nil {
-		log.Error(err, "unable to set up client config")
-		os.Exit(1)
-	}
-
-	// Create a new Cmd to provide shared dependencies and start components
-	log.Info("setting up manager")
+        // Setup a Manager
 	mgr, err := manager.New(cfg, manager.Options{MetricsBindAddress: metricsAddr})
-	if err != nil {
-		log.Error(err, "unable to set up overall controller manager")
-		os.Exit(1)
-	}
+        if err != nil {
+                entryLog.Error(err, "unable to set up overall controller manager")
+                os.Exit(1)
+        }
 
-	log.Info("Registering Components.")
+        cs, err := clientset.NewForConfig(cfg)
+        if err != nil {
+                panic(err)
+        }
 
-	// Setup Scheme for all resources
-	log.Info("setting up scheme")
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Error(err, "unable add APIs to scheme")
-		os.Exit(1)
-	}
+        clusterActuator, err := cluster.NewActuator(cluster.ActuatorParams{
+                ClustersGetter: cs.ClusterV1alpha1(),
+        })
+        if err != nil {
+                panic(err)
+        }
 
-	// Setup all Controllers
-	log.Info("Setting up controller")
-	if err := controller.AddToManager(mgr); err != nil {
-		log.Error(err, "unable to register controllers to the manager")
-		os.Exit(1)
-	}
+        machineActuator, err := machine.NewActuator(machine.ActuatorParams{
+                MachinesGetter: cs.ClusterV1alpha1(),
+        })
+        if err != nil {
+                panic(err)
+        }
 
-	log.Info("setting up webhooks")
-	if err := webhook.AddToManager(mgr); err != nil {
-		log.Error(err, "unable to register webhooks to the manager")
-		os.Exit(1)
-	}
+        // Register our cluster deployer (the interface is in clusterctl and we define the Deployer interface on the actuator)
+        common.RegisterClusterProvisioner("dind", clusterActuator)
 
-	// Start the Cmd
-	log.Info("Starting the Cmd.")
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Error(err, "unable to run the manager")
-		os.Exit(1)
-	}
+        if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+                panic(err)
+        }
+
+        if err := clusterapis.AddToScheme(mgr.GetScheme()); err != nil {
+                panic(err)
+        }
+
+        capimachine.AddWithActuator(mgr, machineActuator)
+
+        capicluster.AddWithActuator(mgr, clusterActuator)
+
+        if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+                entryLog.Error(err, "unable to run manager")
+                os.Exit(1)
+        }
 }
